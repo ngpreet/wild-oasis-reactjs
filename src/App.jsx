d551df09ec005c60afa138094041455d6a0eea9a
ngpreet <ng.navpreet@gmail.com>
2024-08-20 10:52:30 +0530

    reusable styled components Button, Input, Row, Heading

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,39 @@
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+import Heading from "./ui/Heading";
+import Row from "./ui/Row";
+
+const StyledApp = styled.main`
+  padding: 32px;
+`;
+
 function App() {
   return (
     <>
-      <h1>Vite + React</h1>
+      <GlobalStyles />
+      <StyledApp>
+        <Row>
+          <Row type="horizontal">
+            <Heading as="h1">The Wild Oasis</Heading>
+            <div>
+              <Heading as="h2">Check in and out</Heading>
+              <Button>Check In</Button>
+              <Button variation="secondary" size="small">
+                Check Out
+              </Button>
+            </div>
+          </Row>
+          <Row>
+            <Heading as="h3">Form</Heading>
+            <div>
+              <Input placeholder="Guests" />
+              <Input placeholder="Guests" />
+            </div>
+          </Row>
+        </Row>
+      </StyledApp>
     </>
   );
 }
