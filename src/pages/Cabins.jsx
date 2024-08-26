714638ae8f179a62eec5fee3790829dcef669c3e
ngpreet <ng.navpreet@gmail.com>
2024-08-26 10:48:30 +0530

    add new cabin with image

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,8 +1,13 @@
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 import CabinTable from "../features/cabins/CabinTable";
+import Button from "../ui/Button";
+import { useState } from "react";
+import CreateCabinForm from "../features/cabins/CreateCabinForm";
 
 function Cabins() {
+  const [showForm, setShowForm] = useState(false);
+
   return (
     <>
       <Row type="horizontal">
@@ -10,7 +15,11 @@ function Cabins() {
         <p>Filter/Sort</p>
       </Row>
       <Row>
-        <CabinTable></CabinTable>
+        <CabinTable />
+        <Button onClick={() => setShowForm((show) => !show)}>
+          Add new cabin
+        </Button>
+        {showForm && <CreateCabinForm></CreateCabinForm>}
       </Row>
     </>
   );
--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -11,6 +11,36 @@ export async function getCabins() {
   return data;
 }
 
+export async function createCabin(newCabin) {
+  const imageName = `${Math.random()}-${newCabin.image.name}`.replaceAll(
+    "/",
+    ""
+  );
+  const imagePath = `${
+    import.meta.env.VITE_SUPABASE_URL
+  }/storage/v1/object/public/cabin-images/${imageName}`;
+
+  const { data, error } = await supabase
+    .from("cabins")
+    .insert([{ ...newCabin, image: imagePath }])
+    .select();
+
+  if (error) {
+    console.error(error);
+    throw new Error("Cabins could not be created");
+  }
+
+  const { error: storageError } = await supabase.storage
+    .from("cabin-images")
+    .upload(imageName, newCabin.image);
+  if (storageError) {
+    await supabase.from("cabins").delete().eq("id", data.id);
+    throw new Error("Cabin image could not be created. Cabin not created");
+  }
+
+  return data;
+}
+
 export async function deleteCabin(id) {
   const { error } = await supabase.from("cabins").delete().eq("id", id);
 
